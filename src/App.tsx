db87f1906bc15207b42d023d7539abc5d2e0239e
Lidi <lidiana.tdsc@gmail.com>
2021-07-20 22:29:53 -0300

    criação do componente produto

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import Home from './pages/home';
 import Products from './pages/products';
+import './styles/reset.css';
+import './styles/global.css';
+
 
 const App = () => {
   return (
---
+++ src/components/ProductCard/index.tsx
@@ -0,0 +1,24 @@
+import './styles.css';
+
+interface IProduct {
+    Title: string;
+    Year: string;
+    imdbID: string;
+    Type: string;
+    Poster: string;
+}
+
+const ProductCard = (props:IProduct) => {
+    return(
+        <div className="product-card">
+            <img src={props.Poster} alt={props.Title} />
+            <div className="info">
+                <h1>{props.Title}</h1>
+                <h2>{props.Year} [{props.imdbID}]</h2>
+                <h3>{props.Type}</h3>
+            </div>
+        </div>
+    );
+}
+
+export default ProductCard;
